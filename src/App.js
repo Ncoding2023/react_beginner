a7b93604180455cca0bf348c9401c531d8519cfb
Ncoding2023 <133838753+Ncoding2023@users.noreply.github.com>
2024-08-06 18:11:23 +0900

    7.2 Coin Tracker
    20240806 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import { useEffect, useState } from "react";
-function Hello() {
-  // function closeFn() {
-  //   console.log("close :(");
-  // }
-  // function openFn() {
-  //   console.log("open :)");
-  //   return closeFn;
-  // }
-  useEffect(() => {
-    console.log("open :)");
-    return () => console.log("close :(");
-  },[]);
-  useEffect(function (){
-    console.log("open :)");
-    return function() {
-      console.log("close :(");
-    }
-  },[]);
-  return  <h1>Hello</h1>
-}
 
 function App() {
-  const [showing,setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
-  return  (
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const [inputMap, setInputMap] = useState();
+  const selectOnChange = (event) => {
+    const selectedOption = JSON.parse(event.target.value);
+    setInputMap(selectedOption);
+  };
+  // console.log("inputMap ===>>",inputMap);
+
+  return (
     <div>
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={selectOnChange}>
+          {coins.map((coin, index) => (
+            <option value={JSON.stringify(coin)} key={index}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD{" "}
+            </option>
+          ))}
+        </select>
+      )}
+      <hr></hr>
+      {inputMap === undefined ? null : (
+        <input value={inputMap.symbol} readOnly disabled />
+      )}
+
+      {inputMap === undefined ? null : (
+        <input value={inputMap.quotes.USD.price} readOnly disabled />
+      )}
     </div>
   );
 }
-
 export default App;
