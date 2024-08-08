e8d115c1add29a7c44cbfdd7baec495b305b56f0
Ncoding2023 <133838753+Ncoding2023@users.noreply.github.com>
2024-08-08 17:47:04 +0900

    7.6 Parameters

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,8 +24,10 @@ function Home() {
         ) : (
           <div>
             {movies.map((movie) => (
+              
               <Movie
                 key={movie.id}
+                id={movie.id}
                 coverImg={movie.medium_cover_image}
                 title={movie.title}
                 summary={movie.summary}
